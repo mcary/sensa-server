acbc730526d83b79c91754f6c186efe3e71d57f7
Marcel M. Cary <marcel@oak.homeunix.org>
2013-05-14 20:21:10 -0700

    Add passing specs for #starve and #dose actions for completeness
    We have a spec for #feed, but not complete coverage yet.
    
    Add specs for the remaining actions so we have complete coverage.

--- spec/features/pump_control_spec.rb
+++ spec/features/pump_control_spec.rb
@@ -1,13 +1,34 @@
 require 'spec_helper'
 
 describe "FeederController", :type => :feature do
-  it "feeds" do
-    serial = mock(:serial)
+  let(:serial) { mock(:serial) }
+  before :each do
     SerialPort.stub(:new => serial)
+  end
+
+  it "feeds" do
     serial.should_receive(:write).with("y")
     serial.should_receive(:close)
     visit '/'
     click_button 'Feed'
     page.should have_content 'Fed!'
+  end
+
+  it "starves" do
+    serial.should_receive(:write).with("n")
+    serial.should_receive(:close)
+    visit '/'
+    click_button 'Starve'
+    page.should have_content 'Starved :('
+  end
+
+  it "doses" do
+    serial.should_receive(:write).with("y")
+    serial.should_receive(:write).with("n")
+    # We should probably be consistent about closing...
+    #serial.should_receive(:close)
+    visit '/'
+    click_button 'Dose'
+    page.should have_content 'Dosed :)'
   end
 end
