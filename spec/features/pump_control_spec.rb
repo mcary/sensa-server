f7f65719065aa6a3491eea6994969a81e883b684
Marcel M. Cary <marcel@oak.homeunix.org>
2013-06-08 06:15:58 -0700

    Move non-transactional examples to their own describe block
    We're currently running all the feeder specs without transactional
    fixtures, even though it needn't be that way.  Also, we're repeating the
    Dose.destory_all cleanup.
    
    Create a nested describe block to limit scope of disabled transactional
    fixtures move destroy_all to a before block.

--- spec/features/pump_control_spec.rb
+++ spec/features/pump_control_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "FeederController", :type => :feature do
+describe "Feeding", :type => :feature do
   let(:serial) { mock(:serial) }
   before :each do
     SimpleSerialPort.stub(:new => serial)
@@ -22,44 +22,48 @@ describe "FeederController", :type => :feature do
     page.should have_content 'Starved :('
   end
 
-  # Disable transactional fixtures for this example because
-  # the dosing thread runs in a separate transaction.  And we're
-  # not using fixtures anyway.
-  self.use_transactional_fixtures = false
-  it "doses" do
-    Dose.destroy_all
-    serial.should_receive(:write).with("y")
-    visit '/'
-    fill_in "Total quantity", :with => "1.0"
-    fill_in "Number of cycles", :with => "2"
-    fill_in "Pause between cycles", :with => "0.5"
-    click_button 'Dose'
-    page.should have_content 'Dosing...'
-    dose = Dose.first
-    dose.completed_at.should be_nil
-    dose.total_quantity.should == 1.0
-    dose.number_of_cycles.should == 2
-    dose.pause_between_cycles.should == 0.5
-    # After response is sent:
-    serial.should_receive(:write).with("n").ordered
-    serial.should_receive(:write).with("y").ordered
-    serial.should_receive(:write).with("n").ordered
-    sleep 1.5
-    dose.reload.completed_at.to_f.should be_within(0.1).of(DateTime.now.to_f)
-  end
+  describe "Dosing" do
+    # Disable transactional fixtures for this example because
+    # the dosing thread runs in a separate transaction.  And we're
+    # not using fixtures anyway.
+    self.use_transactional_fixtures = false
+    before :each do
+      Dose.destroy_all
+    end
 
-  it "shows past doses" do
-    Dose.destroy_all
-    Dose.create!(:total_quantity => 4.2,
-                 :number_of_cycles => 3,
-                 :pause_between_cycles => 40,
-                 :completed_at => Time.parse('2013-01-01 08:00'))
-    visit '/'
-    page.should have_content 'Total quantity'
-    page.should have_content '4.2'
-    page.should have_content '3'
-    page.should have_content '40'
-    page.should have_content '2013-01-01 16:00'
-    page.should have_no_content '#<Dose' # Fixed but in template
+    it "doses" do
+      serial.should_receive(:write).with("y")
+      visit '/'
+      fill_in "Total quantity", :with => "1.0"
+      fill_in "Number of cycles", :with => "2"
+      fill_in "Pause between cycles", :with => "0.5"
+      click_button 'Dose'
+      page.should have_content 'Dosing...'
+      dose = Dose.first
+      dose.completed_at.should be_nil
+      dose.total_quantity.should == 1.0
+      dose.number_of_cycles.should == 2
+      dose.pause_between_cycles.should == 0.5
+      # After response is sent:
+      serial.should_receive(:write).with("n").ordered
+      serial.should_receive(:write).with("y").ordered
+      serial.should_receive(:write).with("n").ordered
+      sleep 1.5
+      dose.reload.completed_at.to_f.should be_within(0.1).of(DateTime.now.to_f)
+    end
+
+    it "shows past doses" do
+      Dose.create!(:total_quantity => 4.2,
+                   :number_of_cycles => 3,
+                   :pause_between_cycles => 40,
+                   :completed_at => Time.parse('2013-01-01 08:00'))
+      visit '/'
+      page.should have_content 'Total quantity'
+      page.should have_content '4.2'
+      page.should have_content '3'
+      page.should have_content '40'
+      page.should have_content '2013-01-01 16:00'
+      page.should have_no_content '#<Dose' # Fixed but in template
+    end
   end
 end
