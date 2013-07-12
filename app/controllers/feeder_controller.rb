7138b574a330d2883679e8714edc982b2e929b02
Marcel M. Cary <marcel@oak.homeunix.org>
2013-07-12 06:33:26 -0700

    Stop initializing pump in controller constructor
    In controller specs, I can't seem to stub SimpleSerialPort.new soon
    enough: the before(:each) callback runs *after* the controller under
    test is created.  As a result, creating the pump raises an error about
    the serial port not being present.  The test somehow passes anyway,
    but it breaks the final initialization steps of the example which set
    @controller and setup view stubbing details.  The result is these errors
    unless I add some work-arounds to the spec:
    
        @controller is nil: make sure you set it in your test's setup
        method.
    
    and:
    
        NoMethodError: undefined method `original_path_set' for
        nil:NilClass
    
    Create the pump lazily in a private attribute reader of the controller
    and clean up the work-arounds in the spec.  We don't even need to mock
    the SimpleSerialPort because it won't be used in this example.

--- app/controllers/feeder_controller.rb
+++ app/controllers/feeder_controller.rb
@@ -1,30 +1,31 @@
 class FeederController < ApplicationController
-  def initialize
-    super
-    @pump = Pump.new(Rails.configuration.hardware)
-  end
-
   def index
     @doses = Dose.order("id DESC")
   end
 
   def feed
-    @pump.on
+    pump.on
     redirect_to root_path, notice: "Fed!"
   end
 
   def starve
-    @pump.off
+    pump.off
     redirect_to root_path, notice: "Starved :("
   end
 
   def dose
-    Doser.new(Dose, params, @pump, logger).start
+    Doser.new(Dose, params, pump, logger).start
     redirect_to root_path, notice: "Dosing..."
   end
 
   def cancel
-    Doser.new(Dose, params, @pump, logger).cancel
+    Doser.new(Dose, params, pump, logger).cancel
     redirect_to root_path, notice: "Cancelled dose."
+  end
+
+  private
+
+  def pump
+    Pump.new(Rails.configuration.hardware)
   end
 end
--- spec/controllers/feeder_controller_spec.rb
+++ spec/controllers/feeder_controller_spec.rb
@@ -1,33 +1,10 @@
 require 'spec_helper'
 
 describe FeederController do
-  # I don't think I should have to do this, but if I don't, I get an
-  # error:
-  #
-  #    NoMethodError: undefined method `original_path_set' for
-  #    nil:NilClass
-  #
-  render_views
-
   # This should return the minimal set of attributes required to
   # create a valid Feeder.
   def valid_attributes
     {}
-  end
-
-  let(:serial) { mock(:serial) }
-  before :each do
-    SimpleSerialPort.stub(:new => serial)
-  end
-
-  before :each do
-    # I don't think I should have to do this, but if I don't, I get an
-    # error:
-    #
-    #     @controller is nil: make sure you set it in your test's setup
-    #     method.
-    #
-    @controller = FeederController.new
   end
 
   describe "GET index" do
